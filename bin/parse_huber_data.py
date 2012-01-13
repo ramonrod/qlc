2a6128b65e261f2fc0c441c952873972be1cb459
Steven Moran <bambooforest@gmail.com>
2012-01-13 15:18:42 +0100

    updating to python3

--- bin/parse_huber_data.py
+++ bin/parse_huber_data.py
@@ -1,12 +1,9 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-import codecs
 import sys
 import operator # python-sort-a-dictionary-by-value
-
-sys.path.append("../src/qlc/")
-import orthography
-import ngram
+import qlc.orthography
+import qlc.ngram
 
 """
 script to parse the huber data and output ngram matrices
@@ -14,21 +11,22 @@ script to parse the huber data and output ngram matrices
 """
 
 if len(sys.argv) != 4:
-    sys.exit("Call: python parseHuberData.py len_of_ngram path_to_orthographyprofile path_to_huber_data \nE.g: python parseHuberData.py 2 ../data/orthography_profiles/Huber1992.txt counterparts_huber1992.txt")
-
+    print("For example, call:")
+    print("python parse_huber_data.py 2 data/orthography_profiles/huber1992.txt data/concepts_with_counterparts/concepts_with_counterparts_huber1992.txt") 
+    sys.exit(1)
+    
 # load orthography profile to parse linguistic forms from the dictionary
 # load the ngram class to parse and calculate freqs of ngmrams
 # load the huber database dump
 
-o = orthography.OrthographyParser(sys.argv[2])
-n = Ngram.Ngram()
+o = qlc.orthography.OrthographyParser(sys.argv[2])
 
 # start the script
-file = codecs.open(sys.argv[3], "r", "utf-8")
+file = open(sys.argv[3], "r")
 
 file_header = file.readline()
-# COUNTERPART CONCEPT LANGUAGE_BOOKNAME LANGUAGE_CODE FAMILY BIBTEX_KEY
-
+# counterparts header looks like:
+# COUNTERPART CONCEPT LANGUAGE_BOOKNAME LANGUAGE_CODE BIBTEX_KEY
 concept_container = {} 
 data_container = {}
 ngram_container = {}
@@ -42,48 +40,52 @@ for line in file:
     concept = columns[1]
     language_bookname = columns[2]
     language_code = columns[3]
-    family = columns[4]
-    bibtex_key = columns[5]
+    bibtex_key = columns[4]
 
     if language_code == "spa" or language_code == "eng":
         continue
 
     # store the data
-    if not data_container.has_key(language_bookname):
+    if not language_bookname in data_container:
         data_container[language_bookname] = {}
         data_container[language_bookname][concept] = {}
     else:
-        if not data_container[language_bookname].has_key(concept):
+        if not concept in data_container[language_bookname]:
             data_container[language_bookname][concept] = {}
 
     # store the concepts
-    if not concept_container.has_key(concept):
+    if not concept in concept_container:
         concept_container[concept] = 1
     else:
         concept_container[concept] += 1
 
     # store language names
-    if not language_names.has_key(language_bookname):
+    if not language_bookname in language_names:
         language_names[language_bookname] = 1
     else:
         language_names[language_bookname] += 1
 
     # do the orthography parsing
-    orthography_parse = o.parseToIpa(counterpart) # set to parse to Ipa; see also o.parse() for just graphemic parse
-    orthography_parse = orthography_parse.replace("#", "")
-    split_orthography_parse = orthography_parse.split()
-    ngram_hash = n.getNgramsFromList(split_orthography_parse, int(sys.argv[1])) # set length of ngrams
+    orthography_parse_tuple = o.parse_string_to_ipa_string(counterpart) # set to parse to Ipa; see also o.parse() for just graphemic parse
+    orthography_parse = orthography_parse_tuple[1]
+    split_orthography_parse = tuple(orthography_parse.split())
+    ngram_tuple = qlc.ngram.ngrams_from_graphemes(split_orthography_parse, int(sys.argv[1]))
+
+# n.getNgramsFromList(split_orthography_parse, int(sys.argv[1])) 
+
+    print(ngram_tuple)
 
     # keep track of all ngrams across all languages and their counts
-    for k, v in ngram_hash.iteritems():
-        if not ngram_container.has_key(k):
-            ngram_container[k] = v
+    for tuple in ngram_tuple:
+        print(tuple)
+        if not tuple in ngram_container:
+            ngram_container[tuple] = v
         else:
-            ngram_container[k] += v
+            ngram_container[tuple] += v
 
     # populate grams into hash of hash of hash
-    for k, v in ngram_hash.iteritems():
-        if not data_container[language_bookname][concept].has_key(k):
+    for k, v in ngram_hash.items():
+        if not k in data_container[language_bookname][concept]:
             data_container[language_bookname][concept][k] = v
         else:
             data_container[language_bookname][concept][k] += v            
@@ -112,9 +114,9 @@ for line in file:
 
 # print data container
 def printDataContainer():
-    for k, v in data_container.iteritems():
-        print k, v
-    print
+    for k, v in data_container.items():
+        print(k, v)
+    print()
 
 
 # print ngrams by languages
@@ -122,57 +124,57 @@ def printDataContainer():
 # language_names_sorted = sorted(language_names.iteritems(), key=operator.itemgetter(1), reverse=True) 
 # print language_names_sorted
 def printLanguageNgrams():
-    for k, v in language_ngram_sums.iteritems():
-        print k, v
-        print
+    for k, v in language_ngram_sums.items():
+        print(k, v)
+        print()
 
 def printSumGramsByLanguages():
     s = "ngram"
-    for k, v in language_names.iteritems():
+    for k, v in language_names.items():
         s += "\t"+k
-    print s.encode("utf-8")
+    print(s)
 
 
-    for ngram, count in ngram_container.iteritems():
+    for ngram, count in ngram_container.items():
         row = ngram
-        for lang_name, concept_count in language_names.iteritems():
-            if language_ngram_sums[lang_name].has_key(ngram):
+        for lang_name, concept_count in language_names.items():
+            if ngram in language_ngram_sums[lang_name]:
                 sum = language_ngram_sums[lang_name][ngram]
                 row += "\t"+str(sum)
             else:
                 row += "\t"+"0"
-        print row.encode("utf-8").rstrip("\t")
+        print(row.rstrip("\t"))
         
 
 # printSumGramsByLanguages()
 
 def printAllNgrams():    
-    for k, v in ngram_container.iteritems():
-        print k.encode("utf-8"), "\t", v
+    for k, v in ngram_container.items():
+        print(k+"\t"+v)
 
 
 # print everything
 def printGramsByConceptsMatrix():
     s = "lang_name"+"\t"+"ngram"
-    for k, v in concept_container.iteritems():
-        s += "\t"+k.encode("utf-8")
-    print s.rstrip("\t")
+    for k, v in concept_container.items():
+        s += "\t"+k
+    print(s.rstrip("\t"))
 
     # iterate through outter hash in container: language_bookname: {}
     s = ""
-    for language_name, concept_hash in data_container.iteritems():
+    for language_name, concept_hash in data_container.items():
 
         # iterate over all ngrams
-        for ngram, ngram_count in ngram_container.iteritems():
+        for ngram, ngram_count in ngram_container.items():
             s += language_name+"\t"+ngram
             # iterate through the x header of concepts
-            for concept, ngram_hash in concept_container.iteritems():
+            for concept, ngram_hash in concept_container.items():
 
                 # check to see if the language has the concept
-                if data_container[language_name].has_key(concept):
+                if concenpt in data_container[language_name]:
 
                     # if language has concept, check to see if that counterpart has the ngram
-                    if data_container[language_name][concept].has_key(ngram):
+                    if ngram in data_container[language_name][concept]:
                         count = data_container[language_name][concept][ngram]
                         s += "\t"+str(count)
                     else:
@@ -181,7 +183,7 @@ def printGramsByConceptsMatrix():
                 else:
                     s += "\t"+"NA"
                     
-            print s.encode("utf-8").rstrip("\t")
+            print(s.rstrip("\t"))
             s = ""
 
 printGramsByConceptsMatrix()
