c9b86880506f26d4104bc003bc5f1e138753c641
Steve Moran <bambooforest@gmail.com>
2012-04-25 17:04:35 +0200

    added get_orthography_profile path; probably don't need it in the long run

--- src/qlc/__init__.py
+++ src/qlc/__init__.py
@@ -3,3 +3,5 @@ import os
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 def get_data(path):
     return os.path.join(_ROOT, 'data', path)
+def get_orthography_profile(path):
+    return os.path.join(_ROOT, 'data/orthography_profiles', path)
